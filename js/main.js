577854195d539249eb3e4f9430a98c76cf2210a9
Ron Yu <ronyu77@gmail.com>
2015-02-07 17:03:09 +0800

    one bug remain

--- js/main.js
+++ js/main.js
@@ -10,14 +10,38 @@
       title: "problem 2",
       content: "what is JQuery?\n",
       answer: false
+    },{
+      id: 3,
+      title: "problem 3",
+      content: "what is JQuery?\n",
+      answer: false
+    },{
+      id: 4,
+      title: "problem 4",
+      content: "what is JQuery?\n",
+      answer: true
+    },{
+      id: 5,
+      title: "problem 5",
+      content: "what is JQuery?\n",
+      answer: false
     }];
+    var problems = getProblem();
+
     function getProblem(){
       return problems;
     }
+
     function checkAnswer(problemId, userAns){
       //
-      
-      return true;
+      for(var i=0; i<problems.length; i++){
+        if(problems[i].id == problemId){
+          if(problems[i].answer == userAns)
+            return true;
+          else
+            return false;
+        }
+      }
     }
     function gameLogic(result){
       
@@ -25,17 +49,32 @@
       
     }
     function displayResult(result){
-      
-      $('#problem, #result').toggle();
-      
+      if(result == true){
+        $('.true-signal').show();
+        $('.false-signal').hide();
+      }
+      else{
+        $('.true-signal').hide();
+        $('.false-signal').show();
+      }
+      $('#result').toggle();
+      //$('#answer').toggle();
+      setTimeout(displayProblem,1000);
+      setTimeout(function(){
+        $('#result').toggle();
+        //$('#answer').toggle();
+      },1000);
+      // displayProblem();
     }
 
 
     $(function(){
-      var problems = getProblem();
-      var problem = problems.pop();
-      $('#problem .title').text(problem.title);
-      $('#problem .content').text(problem.content);
+
+      // var problems = getProblem();
+      displayProblem(problems);
+      //var problem = problems.pop();
+      // $('#problem .title').text(problem.title);
+      // $('#problem .content').text(problem.content);
       $('#answer button').click(function(){ 
         var result;
         if($(this).attr('id') == 'trueButton'){
@@ -49,4 +88,10 @@
       });
     
     });
+    function displayProblem(){
+        var problem = problems.pop();
+        $('#problem .title').text(problem.title);
+        $('#problem .content').text(problem.content);
+    }
+    
     
