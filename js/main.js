425f43387fb120d00ef52a411d7a9af2de5b6091
judy <weng7687@gmail.com>
2015-02-13 00:47:54 +0800

    Judy debug

--- js/main.js
+++ js/main.js
@@ -4,48 +4,37 @@
       var problems = [{
         pid: 1,
         title: "problem 1",
-        content: "Lorem ipsum , consectetur adipisicing elit. Accusantium, nihil!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, nihil!\n",
+        content: "the answer is true !!Lorem ipsum , consectetur adipisicing elit. Accusantium, nihil!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, nihil!\n",
         answer: true
       },{
         pid: 2,
         title: "problem 2",
-        content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, nihil!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, nihil!\n",
+        content: "the answer is false !!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, nihil!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, nihil!\n",
         answer: false
       },{
         pid: 3,
         title: "problem 3",
-        content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, nihil!\n",
+        content: "the answer is false !!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, nihil!\n",
         answer: false
       },{
         pid: 4,
         title: "problem 4",
-        content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, nihil!\n",
+        content: "the answer is true !!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, nihil!\n",
         answer: true
       },{
         pid: 5,
         title: "problem 5",
-        content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, nihil!\n",
+        content: "the answer is false !!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, nihil!\n",
         answer: false
       }];
 
-    function checkAnswer(problemId, userAns){
-
-      for(var i=0; i<=problems.length; i++){
-        // document.write("userAns = " + userAns + " problemID = " + problemId + " problems[i].id = " + problems[i].id + " String (problems[i].answer) = " + String (problems[i].answer));
-        document.write(" i : " + i);
-        document.write(" id : " + problemId);
-        document.write(" pid : " + problems[i].pid);
-        
-        if(problems[i].pid == problemId){
-            document.write("2 - ");
-          if(String (problems[i].answer) == String(userAns)){
-            document.write("problems[i].id " + String (problems[i].answer) + "String (problems[i].answer)" + String(problems[i].answer));
-            return "true";
-          }else{
-            return "false";
-          }
-        }
-      }
+    function checkAnswer(problemAns, userAns){
+      $('.btn').disabled=true
+      if(String (problemAns) == String(userAns)){  
+        return "true";
+      }else{
+        return "false";
+      }        
     }
     
     function gameLogic(result){      
@@ -68,26 +57,31 @@
       },1000);
       // displayProblem();
     }
-
-
     $(function(){
       // var problems = getProblem();
-      var problemID = PopAProblem();
+      var problemAns = PopAProblem();
+      var result;
       //var problem = problems.pop();
       // $('#problem .title').text(problem.title);
       // $('#problem .content').text(problem.content);
       $('#trueButton').click(function(){
+        result = checkAnswer(problemAns, 'true');
+        // document.write("result " + result);
+        displayResult(result);
+      })
+      $('#falseButton').click(function(){
         var result;
-        result = checkAnswer(problemID, 'true');
+        result = checkAnswer(problemAns, 'false');
         // document.write("result " + result);
         displayResult(result);
       });
     
     });
     function PopAProblem(){
+        $('.btn').disabled=false
         var problem = problems.pop();
         $('#problem .title').text(problem.title);
         $('#problem .content').text(problem.content);
         // $('#problem .content').text(problem.id);
-        return problem.pid;
+        return problem.answer;
     }
